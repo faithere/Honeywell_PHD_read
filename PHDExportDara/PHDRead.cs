863c2432a7a67ccb3b54000c7549a892fd9f0ffb
Marcin Kujawa <biz.marcin@gmail.com>
2017-07-24 16:43:14 +0200

    How to read from Honeywell PHD

---
+++ PHDExportDara/PHDRead.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Uniformance.PHD;
+
+namespace PHDExportData
+{
+    public class PHDRead
+    {
+        public PHDRead()
+        {
+
+        }
+
+        public static void Read (string tag, Tag MyTag, PHDHistorian phd, Double[] timestamps, Double[] values, short[] confidences, string pathOutput)
+        {
+            Console.WriteLine($"{DateTime.Now} - Start PHD query, tag: {tag}");
+            phd.FetchData(MyTag, ref timestamps, ref values, ref confidences);
+            var pathOutput1 = pathOutput + tag + ".txt";
+            var writer = File.CreateText(pathOutput1);
+            Console.WriteLine($"{DateTime.Now} - End PHD query, tag: {tag}");
+            if (timestamps != null)
+            {
+                int count = timestamps.GetUpperBound(0);
+                Console.WriteLine($"{DateTime.Now} - Start writing to file");
+                Console.WriteLine($"{DateTime.Now} - Retrieved {count} data points.");
+
+                using (writer)
+                {
+                    writer.WriteLine($"No; timestamp; value; confidence");
+                    for (int i = 0; i < count; ++i)
+                    {
+                        writer.WriteLine($"{i + 1}) {DateTime.FromOADate(timestamps[i])}, {values[i]}, {confidences[i]}");
+                    }
+                }
+                Console.WriteLine($"{DateTime.Now} - End writing to file");
+                Console.WriteLine();
+                if (count>2000000)
+                {
+                    Console.WriteLine("Wait 1min due to big data package");
+                    Thread.Sleep(15000);
+                    Console.WriteLine();
+                }
+            }
+            
+        }
+
+    }
+}
---
+++ PHDExportDara/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Uniformance.PHD;
+
+namespace PHDExportData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                PHDHistorian phd = new PHDHistorian();
+                phd.DefaultServer = new PHDServer("162.20.0.26", SERVERVERSION.RAPI200);
+                phd.Sampletype = SAMPLETYPE.Raw;
+                phd.ReductionType = REDUCTIONTYPE.None;
+
+                Double[] timestamps = null;
+                Double[] values = null;
+                short[] confidences = null;
+                Console.WriteLine("Path of tag list:");
+                var path = Console.ReadLine();
+                Console.WriteLine("Path of output:");
+                var pathOutput = Console.ReadLine();
+
+                var tagList = new List<string>();
+
+                using (var reader = new StreamReader(path))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var lines = reader.ReadLine();
+                        tagList.Add(lines);
+                    }
+                }
+
+                Console.WriteLine("Start time (ralative time):");
+                phd.StartTime = Console.ReadLine();
+                Console.WriteLine("End time (ralative time):");
+                phd.EndTime = Console.ReadLine();
+                Console.WriteLine("Enter log file name:");
+                var logWrite = File.CreateText(Console.ReadLine());
+                int milliseconds = 60000;
+                using (logWrite)
+                {
+                    foreach (string tag in tagList)
+                    {
+                    Tag MyTag = new Tag(tag);
+
+                        try
+                        {
+                           PHDRead.Read(tag, MyTag, phd, timestamps, values, confidences, pathOutput);
+                           phd.Dispose();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+
+                            logWrite.WriteLine($"{DateTime.Now} - {tag} - {ex.Message}");
+
+                            if (!ex.Message.ToString().Contains("10012") && !ex.Message.ToString().Contains("10006") && !ex.Message.ToString().Contains("10068") && !ex.Message.ToString().Contains("10060"))
+                            {
+                                while (true)
+                                {
+                                    phd.Dispose();
+                                    Thread.Sleep(milliseconds);
+                                    try
+                                    {
+                                        PHDRead.Read(tag, MyTag, phd, timestamps, values, confidences, pathOutput);
+                                        break;
+                                    }
+                                    catch (Exception ex1)
+                                    {
+                                        Console.WriteLine($"{DateTime.Now}: {ex1.Message}");
+                                        logWrite.WriteLine($"{DateTime.Now} - {tag} - {ex1.Message}");
+                                    }
+                                }
+                            }
+                        }
+                        
+
+
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+       
